4b78d4060219194904d722da2d6eded0078d2547
Diamond_Arrow <diamond1arrow@gmail.com>
2025-05-02 17:37:40 +0700

    Added: Client CLI

--- CLI/ClientApp/Program.cs
+++ CLI/ClientApp/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+using Lab3.Models;
+
+class ClientApp
+{
+    const int port = 5000;
+
+    static void Main()
+    {
+        ClientHandler clientHandler = new ClientHandler(port);
+
+        Console.Write("Введите IP-адрес сервера: ");
+        string ip = Console.ReadLine() ?? throw new ArgumentNullException(nameof(ip));
+
+        using var client = new TcpClient();
+        client.Connect(ip, port);
+        using var stream = client.GetStream();
+
+        // Получение списка дисков
+        string drives = clientHandler.ReceiveMessage(stream);
+        Console.WriteLine("Доступные диски: " + drives);
+
+        Console.Write("Введите путь к каталогу или файлу: ");
+        string path = Console.ReadLine() ?? throw new ArgumentNullException(nameof(ip));
+        clientHandler.SendMessage(stream, path);
+
+        string response = clientHandler.ReceiveMessage(stream);
+        Console.WriteLine("Ответ от сервера:\n" + response);
+    }
+}
