d2a5df1ee7db119d46968e7585c4c802c0322eaf
Diamond_Arrow <diamond1arrow@gmail.com>
2025-05-02 19:30:40 +0700

    Changed: Handlers are now in a different forlders

---
+++ Core/Models/Handlers/ServerHandler.cs
@@ -0,0 +1,28 @@
+namespace Lab3.Models;
+
+using System.Net.Sockets;
+using System.Text;
+using Lab3.Interfaces;
+
+public class ServerHandler: ISender, IReceiver
+{
+    public int Port { get; }
+
+    public ServerHandler(int port)
+    {
+        Port = port;
+    }
+
+    public void SendMessage(NetworkStream stream, string message)
+    {
+        byte[] data = Encoding.UTF8.GetBytes(message);
+        stream.Write(data, 0, data.Length);
+    }
+
+    public string ReceiveMessage(NetworkStream stream)
+    {
+        byte[] buffer = new byte[8192];
+        int bytes = stream.Read(buffer, 0, buffer.Length);
+        return Encoding.UTF8.GetString(buffer, 0, bytes);
+    }
+}
